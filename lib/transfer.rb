d26a048b3890fffb365e29c6c720d674ab8e5b3f
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 20:07:35 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,6 +18,23 @@ class Transfer
   end
   
   def execute_transaction
-    
+    if @sender.balance > @amount && @status == 'pending' && @sender.valid? && @receiver.valid?
+      @sender.balance -= @amount
+      @receiver.balance += @amount
+      @status = 'complete'
+    else 
+      @status = "rejected"
+      return "Transaction rejected. Please check your account balance."
+    end
+  end
+  
+  def reverse_transfer
+    if @status == 'complete'
+      @sender.balance += @amount
+      @receiver.balance -= @amount
+      @status = 'reversed'
+    else
+      nil 
+    end
   end
 end
