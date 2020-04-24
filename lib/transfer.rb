ed8e8ed083c5eec783aa362213f1cab489de64b0
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 19:58:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,11 @@
 class Transfer
-  attr_accessor :sender, :receiver
-
-
-  def initialize(sender, receiver)
-    @sender = sender
+ attr_accessor :receiver, :sender, :amount, :status
+  
+  def initialize(sender, receiver, amount=0)
     @receiver = receiver
+    @sender = sender
+    @amount = amount
+    @status = 'pending'
   end
- 
  
 end
