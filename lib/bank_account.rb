bc16eadf42b241b8294a01a46025c367ed849593
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 18:46:04 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,18 @@
 class BankAccount
-
+  attr_accessor :balance, :status
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+  
+  def deposit
+    
+  end
+  
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
 end
