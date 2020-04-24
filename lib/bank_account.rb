1be2ce3a7c9c9f4da7610339f4d9c18801fbad82
Dina Kendle <dina.kendle@gmail.com>
2020-04-24 19:50:07 +0000

    Done.

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -8,11 +8,23 @@ class BankAccount
     @status = "open"
   end
   
-  def deposit
-    
+  def deposit(deposit_amount)
+    @balance += deposit_amount
   end
   
   def display_balance
     "Your balance is $#{self.balance}."
+  end
+  
+  def valid?
+    if @status == "open" && @balance > 0
+      true 
+    else 
+      false 
+    end
+  end
+  
+  def close_account
+    @status = "closed"
   end
 end
